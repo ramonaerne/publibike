e55daba08c50142da246022ba06992402c658ee2
Ramon Aerne <raerne@slab1vrt.ethz.ch>
2018-05-29 10:00:51 +0200

    working script

--- setup_cron.sh
+++ setup_cron.sh
@@ -5,13 +5,15 @@ mkdir -p data
 mkdir -p logs
 
 # setup crontab
-CWD="$(pwd)"
-NUM_CRONTABS="$(crontab -l | sed 's/^ *//;/^[*@0-9]/!d' | wc -l)"
-
+# it seems that the cronjobs get deleted at 0700 everyay
+# therefore call this script from remote cron at 0701
+# also make sure it is run from cronjob on this machine couple of times
+# to make sure that it doesn't have too big gaps in case sth gets deleted
+CWD=$(dirname -- "$(readlink -f "$0")")
+NUM_CRONTABS="$(crontab -l 2> /dev/null | sed 's/^ *//;/^[*@0-9]/!d' | wc -l)"
 if [ ${NUM_CRONTABS} -lt 1 ]; then
-	echo "*/5 * * * * python3 ${CWD}/crawl_once.py >> ${CWD}/logs/cron_output.log 2>&1" > mycron
+	echo "3 */3 * * * ${CWD}/setup_cron.sh" > mycron
+	echo "*/5 * * * * python3 ${CWD}/crawl_once.py >> ${CWD}/logs/cron.log 2>&1" >> mycron
 	crontab mycron
 	rm mycron
-else
-	echo "crontabs already running, don't create one"
 fi
